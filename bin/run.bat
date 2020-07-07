c9c77dd6e1b52a0771548d615e8621d9f02199f6
hellolinux <hellolinux@qq.com>
2020-07-07 22:15:04 +0800

    import executable file

---
+++ bin/run.bat
@@ -0,0 +1,5 @@
+:Ncstudio.string   		中文Ncstudio.string的路径文件名
+:Ncstudio_Rus.string	要保存的俄语Ncstudio_Rus.string的路径文件名
+:translation.xlsx		xlsx的路径文件名, 不允许是中文
+readExcel.exe ./Ncstudio.string  ./Ncstudio_Rus.string  ./translation.xlsx
+@pause
